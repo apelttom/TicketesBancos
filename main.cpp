4f00ff6ee68d05829df3f50cc2f36cf56d6a2cdb
FelipeM23 <felipe_m_16@hotmail.com>
2014-04-02 19:34:02 -0600

    Merge
    merge

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main()
 {
+<<<<<<< HEAD
     // Here we will start our application
+=======
+    cout << "Hello world!" << endl;
+>>>>>>> parent of fc1f143... Hello world
     return 0;
 }
