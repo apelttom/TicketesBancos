c465bc132c175c56545176fcfcb6af84381c5d3b
FelipeM23 <felipe_m_16@hotmail.com>
2014-04-02 19:22:36 -0600

    Correction
    cambios guardados

--- main.cpp
+++ main.cpp
@@ -5,10 +5,7 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
-<<<<<<< HEAD
     cout << "SSSSSHello Thomas!" << endl;
-=======
     cout << "No hello Thomas!" << endl;
->>>>>>> 476a1004dab6c59d65a610cca8bb707fceab2173
     return 0;
 }
