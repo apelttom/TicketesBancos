7f5f3c54dbd08506e2bc67f35771499d79160fbd
Tom Apeltauer <t.apeltauer@seznam.cz>
2014-04-02 19:34:27 -0600

    RESTART PROJECT
    Restarting project

--- main.cpp
+++ main.cpp
@@ -4,10 +4,7 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
     // Here we will start our application
-=======
     cout << "Hello world!" << endl;
->>>>>>> parent of fc1f143... Hello world
     return 0;
 }
