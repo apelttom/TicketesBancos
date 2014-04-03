06e8e096a3bb65ecce0e87997327ea45b21812c3
Tom Apeltauer <t.apeltauer@seznam.cz>
2014-04-02 19:22:06 -0600

    Correction Tomas
    Correcting error

--- main.cpp
+++ main.cpp
@@ -5,10 +5,6 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
-<<<<<<< HEAD
-    cout << "SSSSSHello Thomas!" << endl;
-=======
     cout << "No hello Thomas!" << endl;
->>>>>>> 476a1004dab6c59d65a610cca8bb707fceab2173
     return 0;
 }
